c31c2bc83550414be09da636cefc496bce6200bf
kaio <kaioeduardo401@outlook.com>
2024-05-28 16:11:19 -0300

    fix: warning update carrinho

--- obj/Debug/net8.0/ProjetoPixelPlace.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoPixelPlace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5ef2f8e790048222979e3606c2c69c315d4873")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
