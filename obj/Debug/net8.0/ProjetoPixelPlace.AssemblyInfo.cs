bbed59c2765ffd083a9ce2bc9cac717d4074c84e
KaioBorgesDev <kaioeduardo401@outlook.com>
2024-05-29 15:20:44 -0300

    fix- backend details view jogo

--- obj/Debug/net8.0/ProjetoPixelPlace.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoPixelPlace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c1477ffb18c188aa0b34ba5e0e087410eeae82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6b647cfe694e3a8019a1957cf23b6aa6ff8f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
